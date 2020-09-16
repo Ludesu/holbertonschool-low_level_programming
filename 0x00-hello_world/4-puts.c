226e4b1a41d4c1f54aa7e4d2d7a34a6a3723e2d1
Ludesu <yasser.toumi@hotmail.com>
2020-09-16 12:17:19 +0100

    _

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* main does something */
 int main(void)
 {
-	puts("\"Programlolming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
